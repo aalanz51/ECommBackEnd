7eb224b7c15f1796124fc505bd5ab6f24a486cce
Alexander Lanz <austinalexanderlanz@gmail.com>
2022-03-24 23:50:10 -0400

    models completed

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,16 +1,33 @@
-// import models
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
 
-// Categories have many Products
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
-// Products belongToMany Tags (through ProductTag)
 
-// Tags belongToMany Products (through ProductTag)
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
+
+
+Product.belongsToMany(Tag, {
+  through:{
+    model: ProductTag
+  }
+});
+
+
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag
+  }
+});
+
 
 module.exports = {
   Product,
